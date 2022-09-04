f223f68dfe1ca6ec5a44df7a74057aac9c5d7f1e
WaToArt <93100367+WaToArt@users.noreply.github.com>
2022-09-04 14:19:33 -0700

    TEST: > added test_unit_lists.py

---
+++ tests/test_unit_lists.py
@@ -0,0 +1,13 @@
+import pytest
+from pytest_django import asserts
+
+from django.urls import resolve
+
+from lists.views import home_page
+
+class Tests_HomePage:
+
+    def test_root_url_resolves_to_home_page_views(self):
+        found = resolve('/')
+
+        assert found.func == home_page()
