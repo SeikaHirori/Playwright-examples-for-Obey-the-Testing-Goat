659ac65520b93300666ce8ee20ad4e64699671b8
WaToArt <93100367+WaToArt@users.noreply.github.com>
2022-09-04 12:36:13 -0700

    FIXED: - Structure of pytest setup

--- tests/test_functional.py
+++ tests/test_functional.py
@@ -3,8 +3,10 @@ import pytest
 import re
 from playwright.sync_api import Page, expect
 
-class Test_homepage:
+class Tests_homepage:
 
+    def test_this(self):
+        assert 0 == None
 
     def test_title(self, page: Page):
         page.goto("http://localhost:8000")
