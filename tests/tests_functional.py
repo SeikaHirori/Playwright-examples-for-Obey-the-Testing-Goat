b06ce02db38cc8e26c43a803b7453b260f207fed
WaToArt <93100367+WaToArt@users.noreply.github.com>
2022-09-04 12:31:28 -0700

    TEST: - wrote, but pytest are not picking up

---
+++ tests/tests_functional.py
@@ -0,0 +1,12 @@
+import pytest
+
+import re
+from playwright.sync_api import Page, expect
+
+class Test_homepage:
+
+
+    def test_title(self, page: Page):
+        page.goto("http://localhost:8000")
+
+        expect(page).to_have_title(re.compile("Django"))
