5690de1b5d43a67d365f50a8ea0347e8b8ffe313
WaToArt <93100367+WaToArt@users.noreply.github.com>
2022-09-06 14:08:47 -0700

    TDD: > Added "HttpResponse()"

--- lists/views.py
+++ lists/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpRequest
+from django.http import HttpRequest, HttpResponse
 
 # Create your views here.
 
 def home_page(request:HttpRequest):
-    pass
+    return HttpResponse()
