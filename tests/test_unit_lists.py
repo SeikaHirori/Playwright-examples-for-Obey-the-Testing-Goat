1ac90403189c480a40d0802afbb498dadcbf884a
WaToArt <93100367+WaToArt@users.noreply.github.com>
2022-09-08 12:00:48 -0700

    REFACTORED: > Removed old versions' code that isn't needed when using django.test's Client()

--- tests/test_unit_lists.py
+++ tests/test_unit_lists.py
@@ -1,5 +1,6 @@
 import pytest
 from pytest_django.asserts import assertTemplateUsed
+from pytest_django import asserts
 
 from django.urls import resolve
 from django.http import HttpRequest, HttpResponse
@@ -39,12 +40,9 @@ class Tests_HomePage:
     def test_home_page_returns_correct_html_v3(self): # Uses Django.test's Client()
         response:HttpResponse = Client().get('/')
 
-        html = response.content.decode('utf8')
-        
-        assert html.startswith('<html>') == True
-        assert '<title>To-Do lists</title>' in html
-        assert html.strip().endswith('</html>') == True
-
+        ### Directly importing pytest_django's asserts
         assertTemplateUsed(response, 'lists/home.html')
 
+        ### Example of how to call pytest_django's asserts; This easily provides a list of available option
+        # asserts.assertTemplateUsed(response, 'lists/home.html')
         
