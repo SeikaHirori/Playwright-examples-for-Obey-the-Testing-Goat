050309d43ac70dc4d4fb9ab7b089210f04d6067f
WaToArt <93100367+WaToArt@users.noreply.github.com>
2022-09-08 13:55:19 -0700

    TDD FAIL: - wrong request.POST is used; it doesn't account if POST is empty

--- lists/views.py
+++ lists/views.py
@@ -4,7 +4,11 @@ from django.http import HttpRequest, HttpResponse
 # Create your views here.
 
 def home_page(request:HttpRequest):
-    if request.method == 'POST':
-        return HttpResponse(request.POST['item_text']) # TODO
+    # if request.method == 'POST':
+    #     return HttpResponse(request.POST['item_text']) # TODO
 
-    return render(request, 'lists/home.html')
+    # return render(request, 'lists/home.html')
+
+    return render(request, 'lists/home.html', {
+        'new_item_text': request.POST['item_text'],
+    })
