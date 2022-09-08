f33c2ce40c748a9d7a98ff76d98429b98ff4effb
WaToArt <93100367+WaToArt@users.noreply.github.com>
2022-09-08 12:02:40 -0700

    REFACTORED: > Removed redunant tests

--- tests/test_unit_lists.py
+++ tests/test_unit_lists.py
@@ -13,30 +13,6 @@ from lists.views import home_page
 
 class Tests_HomePage:
 
-    def test_root_url_resolves_to_home_page_views(self):
-        found = resolve('/')
-
-        assert found.func == home_page
-
-    def test_home_page_returns_correct_html_v1(self):
-        request = HttpRequest()
-        response:HttpResponse = home_page(request)
-        html = response.content.decode('utf8')
-        
-        assert html.startswith('<html>') == True
-
-        assert "<title>To-Do lists</title>" in html
-
-        assert html.strip().endswith('</html>') == True
-    
-    def test_home_page_returns_correct_html_v2(self): # Uses render_to_string
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode('utf8')
-        expected_html = render_to_string('lists/home.html')
-
-        assert html == expected_html
-
     def test_home_page_returns_correct_html_v3(self): # Uses Django.test's Client()
         response:HttpResponse = Client().get('/')
 
@@ -45,4 +21,6 @@ class Tests_HomePage:
 
         ### Example of how to call pytest_django's asserts; This easily provides a list of available option
         asserts.assertTemplateUsed(response, 'lists/home.html')
+
+
         
