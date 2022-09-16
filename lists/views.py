b79312382c4d6251362137e7ffef579b17dcb902
WaToArt <93100367+WaToArt@users.noreply.github.com>
2022-09-16 14:24:47 -0700

    FAILED: - Attempt to use "sync_to_async" failed - Error: "django.core.exceptions.SynchronousOnlyOperation: You cannot call this from an async context - use a thread or sync_to_async."

--- lists/views.py
+++ lists/views.py
@@ -3,9 +3,7 @@ from django.http import HttpRequest, HttpResponse
 
 from lists.models import Item
 
-
 # Create your views here.
-
 def home_page(request: HttpRequest):
     if request.method == 'POST':
         Item.objects.create(text=request.POST['item_text'])
